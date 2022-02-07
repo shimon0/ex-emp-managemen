4e96c3b461c5b6e5e40ff32eeebe0f877b119f1a
shimon0 <shimon.itakura@rakus-partners.co.jp>
2022-02-07 17:40:03 +0900

    login make

--- ex-emp-management/src/main/java/jp/co/sample/controller/AdministratorController.java
+++ ex-emp-management/src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +19,10 @@ import jp.co.sample.service.AdministratorService;
 public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
+	
+
+	@Autowired
+	private HttpSession session;
 	/**
 	 * 
 	 * @return 従業員パラメータ
@@ -60,5 +67,16 @@ public class AdministratorController {
 	public String toLogin() {
 		return "administrator/login";
 	}
-	
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if (administrator == null) {
+			model.addAttribute("error", "メールアドレスまたはパスワードが不正です。");
+			return toLogin();
+		}else {
+		
+		session.setAttribute("administratorName", administrator.getName());
+		return "forward:/employee/showList";
+		}
+	}
 }
