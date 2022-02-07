0cfbef3d27c6c96f6f66a0734018704b8b49d896
shimon0 <shimon.itakura@rakus-partners.co.jp>
2022-02-07 15:58:20 +0900

    html make

---
+++ ex-emp-management/src/main/java/jp/co/sample/form/AdministratorController.java
@@ -0,0 +1,61 @@
+package jp.co.sample.form;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Administrator;
+import jp.co.sample.service.AdministratorService;
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+	@Autowired
+	private AdministratorService administratorService;
+	/**
+	 * 
+	 * @return 従業員パラメータ
+	 */
+	@ModelAttribute 
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+	/**
+	 * 
+	 * @return ログイン情報
+	 */
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	/**
+	 * 
+	 * @return administrator/insert.htmlへフォワード
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+	}
+	/**
+	 * 
+	 * @param form 管理者情報
+	 * @return ログイン画面へリダイレクト
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/";
+	}
+	/**
+	 * 
+	 * @return ログイン画面へフォワード
+	 */
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+	}
+}
