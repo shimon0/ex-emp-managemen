74f09cc3a94801287707116f86755ac568837f7b
shimon0 <shimon.itakura@rakus-partners.co.jp>
2022-02-07 15:33:17 +0900

    first commit

---
+++ ex-emp-management/src/main/java/jp/co/sample/service/AdministratorService.java
@@ -0,0 +1,26 @@
+package jp.co.sample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.domain.Administrator;
+import jp.co.sample.repository.AdministratorRepository;
+
+/**
+ * @author smone
+ *
+ */
+@Service
+@Transactional
+public class AdministratorService {
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	/**
+	 * 
+	 * @param administrator 管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
+}
