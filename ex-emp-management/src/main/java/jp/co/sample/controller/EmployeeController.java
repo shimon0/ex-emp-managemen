e4bc8361c8f684f2077493b64d61762c521bbdd5
shimon0 <shimon.itakura@rakus-partners.co.jp>
2022-02-10 14:46:53 +0900

    detail

--- ex-emp-management/src/main/java/jp/co/sample/controller/EmployeeController.java
+++ ex-emp-management/src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -20,6 +22,11 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
 	/**
 	 * @param model
 	 * @return
@@ -29,5 +36,17 @@ public class EmployeeController {
 		List<Employee>employeeList = employeeService.showList();
 		model.addAttribute("employeeList",employeeList);
 		return "employee/list";
+	}
+	/**
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		
+		return "employee/detail";
 	}
 }
